2da3cdd694c852dadcd0c515cce0200633f9d5b3
Lokesh <mlokeshkumara301@gmail.com>
2025-08-13 06:29:55 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/BookCrudApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCrudApplication.class, args);
+		System.out.println("Application Started");
+	}
+
+}
